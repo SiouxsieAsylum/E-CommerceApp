7a6e104dc1028205b986549d26e3b305c54a0da2
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2020-06-26 09:07:51 -0400

    reset.css

---
+++ src/components/homepage/homepage.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const Homepage = () => (
+    <div
+     className="homepage">
+        <div
+        className="directory-menu">
+            <div
+            className="menu-item high">
+                    <div
+                    className="content">
+                        <h1 
+                        className="title">HATS</h1>
+                        <span 
+                        className="subtitle">SHOP NOW</span>
+                    </div>
+            </div>
+            <div
+            className="menu-item high">
+                    <div
+                    className="content">
+                        <h1 
+                        className="title">HATS</h1>
+                        <span 
+                        className="subtitle">SHOP NOW</span>
+                    </div>
+            </div>
+            <div
+            className="menu-item high">
+                    <div
+                    className="content">
+                        <h1 
+                        className="title">HATS</h1>
+                        <span 
+                        className="subtitle">SHOP NOW</span>
+                    </div>
+            </div>
+            <div
+            className="menu-item low">
+                    <div
+                    className="content">
+                        <h1 
+                        className="title">HATS</h1>
+                        <span 
+                        className="subtitle">SHOP NOW</span>
+                    </div>
+            </div>
+            <div
+            className="menu-item low">
+                    <div
+                    className="content">
+                        <h1 
+                        className="title">HATS</h1>
+                        <span 
+                        className="subtitle">SHOP NOW</span>
+                    </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Homepage;
